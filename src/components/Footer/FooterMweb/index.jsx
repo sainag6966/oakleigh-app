fe049ffa115cebd91a922ef69b7ac91fd595e5f7
Sai Nagarjuna <sai@cda.group>
2023-11-30 15:44:21 +0530

    refactor error

--- src/components/Footer/FooterMweb/index.jsx
+++ src/components/Footer/FooterMweb/index.jsx
@@ -3,10 +3,10 @@ function FooterMweb() {
     return (
         <div className="bg-footerBg w-full h-[1150px] text-textPrimary">
             <div className="flex flex-col items-center justify-center gap-10 w-full h-auto">
-                {data1.map((link) => {
+                {data1.map((link,index) => {
                     return (
                         <div className="flex gap-2">
-                            <div>{link}</div>
+                            <div key={index}>{link}</div>
                             <div>+</div>
                         </div>
                     )
