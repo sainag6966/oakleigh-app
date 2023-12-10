0e40781971c7d5e41cb3cd0398728d2129acf76c
Sai Nagarjuna <95867023+sainag6966@users.noreply.github.com>
2023-12-10 13:05:00 +0530

    usp block mobile commit

--- src/components/ContentBlocks/UspBlock/index.jsx
+++ src/components/ContentBlocks/UspBlock/index.jsx
@@ -1,24 +1,33 @@
 import NextImage from "@/reuseComps/NextImage";
+import { useMediaQuery } from "react-responsive";
 function UspBlock() {
   const icon1 = "/Images/Sample/uspIcon1.svg";
   const icon2 = "/Images/Sample/uspIcon2.svg";
   const icon3 = "/Images/Sample/uspIcon3.svg";
+  const isDesktop = useMediaQuery({ query: '(min-width:900px)' })
   const data = [
-    { label: "Free Delivery & Returns", icon: icon1 },
+    // { label: "Free Delivery & Returns", icon: icon1 },
     { label: "Authentic & Certified", icon: icon2 },
-    { label: "Serviced & Guaranteed", icon: icon3 },
+    // { label: "Serviced & Guaranteed", icon: icon3 },
   ];
 
   return (
-    <div className="bg-uspBlockBackground flex w-full justify-around h-[115px]">
+    <div className="bg-uspBlockBackground flex w-full justify-between h-[115px]">
       {data.map((e, index) => {
         return (
-          <div key={index} className="flex items-center justify-center gap-8">
-            <NextImage src={e.icon} width="40" height="24" alt="uspIcon" />
-            <p className="text-[20px]">{e.label}</p>
-            {data.length - 1 !== index && (
-              <div className="min-w-[70px] h-[0.5px] bg-black"></div>
-            )}
+          <div
+            key={index}
+            className="w-full flex items-center justify-between gap-4"
+          >
+            <div className="min-w-[30px] h-[0.5px] bg-black grow-[0.4]"/>
+            <div className="flex items-center justify-center gap-5 w-auto">
+              <NextImage src={e.icon} width="40" height="24" alt="uspIcon" />
+              <p className="text-[20px]">{e.label}</p>
+            </div>
+            <div className="min-w-[30px] h-[0.5px] bg-black grow-[0.4]"/>
+            {/* {data.length - 1 !== index && (
+              <div className="min-w-[30px] h-[0.5px] bg-black"></div>
+            )} */}
           </div>
         );
       })}
