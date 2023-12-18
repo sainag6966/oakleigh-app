7d924659d1dc24cb04bc7925aece4eaf07a65623
Sai Nagarjuna <95867023+sainag6966@users.noreply.github.com>
2023-12-18 15:02:46 +0530

    final commit -1 dangerously html

--- src/components/ContentBlocks/PromiseBlock/index.jsx
+++ src/components/ContentBlocks/PromiseBlock/index.jsx
@@ -7,24 +7,28 @@ function PromiseBlock({ trayData }) {
   const bodyText = trayData?.top_body_text
 
   return (
-    <div className="dxl:px-[140px] px-9">
-      <div className="dxl:mt-[120px] mt-[60px] flex h-[896px] w-full items-center justify-center bg-footerBg p-5 lg:h-[696px]">
-        <div className="dxl:gap-[144px] flex h-full w-full flex-col items-center justify-around gap-8 border-[1px] border-copyRightBorder p-[30px] lg:flex-row lg:gap-12 lg:p-20">
+    <div className="px-9 dxl:px-[140px]">
+      <div className="mt-[60px] flex h-[896px] w-full items-center justify-center bg-footerBg p-5 lg:h-[696px] dxl:mt-[120px]">
+        <div className="flex h-full w-full flex-col items-center justify-around gap-8 border-[1px] border-copyRightBorder p-[30px] lg:flex-row lg:gap-12 lg:p-20 dxl:gap-[144px]">
           <div className=" flex w-full flex-col items-start justify-between gap-6 lg:max-w-[50%] lg:justify-center">
             <img
               src={promiseIcon}
               alt="promise"
-              className="dxl:w-[52px] dxl:h-[66px] h-[47px] w-[37px]"
+              className="h-[47px] w-[37px] dxl:h-[66px] dxl:w-[52px]"
             />
-            <p className="dxl:text-display-15 text-display-13 text-textPrimary">
+            <p className="text-display-13 text-textPrimary dxl:text-display-15">
               {title}
             </p>
-            <div className="dxl:text-display-6 font-sans text-display-3 text-textPrimary">
-              {bodyText}
+            <div className="font-sans text-display-3 text-textPrimary dxl:text-display-6">
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `<ul class="list-disc">${bodyText}</ul>`,
+                }}
+              />
             </div>
           </div>
           <div className="flex h-full w-full items-center justify-center lg:w-1/2">
-            <div className="dxl:h-full relative flex h-[294px] w-full items-center justify-center lg:h-[400px]">
+            <div className="relative flex h-[294px] w-full items-center justify-center lg:h-[400px] dxl:h-full">
               <Image
                 src={promiseImg}
                 layout="fill"
