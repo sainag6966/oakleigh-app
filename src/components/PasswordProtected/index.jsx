1a90cb58c0e1d3aa3c49b0be81121459c9a72349
Sai Nagarjuna <95867023+sainag6966@users.noreply.github.com>
2024-01-30 15:31:06 +0530

    password change

--- src/components/PasswordProtected/index.jsx
+++ src/components/PasswordProtected/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const PasswordProtection = ({ children }) => {
   const [password, setPassword] = useState('')
-  const [isPasswordCorrect, setIsPasswordCorrect] = useState(true)
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState(false)
   const correctPassword = '' // Replace with your atruectual password
 
   const handleSubmit = (e) => {
