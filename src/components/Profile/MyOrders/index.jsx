bc18d0c60e0d4a67e23e25cf30dfe8c766ea50ca
Sai Nagarjuna <95867023+sainag6966@users.noreply.github.com>
2024-02-21 15:19:09 +0530

    arrows added for date and product title

--- src/components/Profile/MyOrders/index.jsx
+++ src/components/Profile/MyOrders/index.jsx
@@ -1,4 +1,7 @@
+import ProgressiveImageComp from '@/reuseComps/ProgressiveImageComp'
 function MyOrders() {
+  const sortIconUp = '/Images/sortIconUp.svg'
+  const sortIconDown = '/Images/sortIconDown.svg'
   const data = [
     {
       accountDetail: '09330550',
@@ -46,8 +49,32 @@ function MyOrders() {
           <thead>
             <tr className="border-b-[1px] border-orderSummaryBorder text-left text-display-5 leading-none dxl:text-display-16">
               <th className="pb-3 pr-5 dxl:pb-5">Account Details</th>
-              <th className="pb-3 pr-5  dxl:pb-5">Date Purchased</th>
-              <th className="pb-3 pr-5  dxl:pb-5">Product Title</th>
+              <th className="flex items-center gap-1 pb-3 pr-5 dxl:pb-5">
+                <p>Date Purchased</p>
+                <section className="flex flex-col gap-[2px]">
+                  {' '}
+                  <section className="h-[5px] w-[5px] cursor-pointer">
+                    <ProgressiveImageComp src={sortIconUp} alt="arrow" />
+                  </section>
+                  <section className="curso h-[5px] w-[5px]">
+                    <ProgressiveImageComp src={sortIconDown} alt="arrow" />
+                  </section>
+                </section>
+              </th>
+              <th className="pb-3 pr-5 dxl:pb-5">
+                <section className="flex items-center gap-1">
+                  <p>Product Title</p>
+                  <section className="flex flex-col gap-[2px]">
+                    {' '}
+                    <section className="h-[5px] w-[5px] cursor-pointer">
+                      <ProgressiveImageComp src={sortIconUp} alt="arrow" />
+                    </section>
+                    <section className="curso h-[5px] w-[5px]">
+                      <ProgressiveImageComp src={sortIconDown} alt="arrow" />
+                    </section>
+                  </section>
+                </section>
+              </th>
               <th className="pb-3 dxl:pb-5 ">Amount Paid</th>
               {/* <th className="p-3">Shop Similar</th> */}
             </tr>
