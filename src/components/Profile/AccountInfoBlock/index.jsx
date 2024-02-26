d06ad8cf30d09f153bd5c85a1b68de99a58798f7
Sai Nagarjuna <95867023+sainag6966@users.noreply.github.com>
2024-02-26 13:53:55 +0530

    few changes made for the accunt info sec

--- src/components/Profile/AccountInfoBlock/index.jsx
+++ src/components/Profile/AccountInfoBlock/index.jsx
@@ -1,4 +1,207 @@
+import { useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
 function AccountInfoBlock() {
+  const formFields = [
+    { name: 'first_name', placeholder: 'First name', value: '', errorMsg: '' },
+    { name: 'last_name', placeholder: 'Last name', value: '', errorMsg: '' },
+    { name: 'email', placeholder: 'Email address', value: '', errorMsg: '' },
+    { name: 'phone', placeholder: 'Phone number', value: '', errorMsg: '' },
+    {
+      name: 'newPassword',
+      placeholder: 'New Password',
+      value: '',
+      errorMsg: '',
+    },
+    {
+      name: 'confirmPassword',
+      placeholder: 'Confirm New Password',
+      value: '',
+      errorMsg: '',
+    },
+  ]
+  const [formData, setFormData] = useState(formFields)
+  const istabScreen = useMediaQuery({ query: '(min-width:600px)' })
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const { name, value } = e.target
+    if (name === 'first_name') {
+      if (!value) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'First name is required' }
+              : field,
+          ),
+        )
+      } else if (!/^[a-zA-Z\s]+$/.test(value)) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'Please enter only alphabets' }
+              : field,
+          ),
+        )
+      } else {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name ? { ...field, value, errorMsg: '' } : field,
+          ),
+        )
+      }
+    }
+    if (name === 'last_name') {
+      if (!value) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'Last name is required' }
+              : field,
+          ),
+        )
+      } else if (!/^[a-zA-Z\s]+$/.test(value)) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'Please enter only alphabets' }
+              : field,
+          ),
+        )
+      } else {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name ? { ...field, value, errorMsg: '' } : field,
+          ),
+        )
+      }
+    }
+    if (name === 'email') {
+      if (!value) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'Email address is required' }
+              : field,
+          ),
+        )
+      } else if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value)) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? {
+                  ...field,
+                  value,
+                  errorMsg: 'Please enter a valid email address',
+                }
+              : field,
+          ),
+        )
+      } else {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name ? { ...field, value, errorMsg: '' } : field,
+          ),
+        )
+      }
+    }
+    if (name === 'phone') {
+      if (!value) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'Phone number is required' }
+              : field,
+          ),
+        )
+      } else if (!/^\d+$/.test(value)) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? {
+                  ...field,
+                  value,
+                  errorMsg: 'Please enter a valid phone number (digits only)',
+                }
+              : field,
+          ),
+        )
+      } else {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name ? { ...field, value, errorMsg: '' } : field,
+          ),
+        )
+      }
+    }
+    if (name === 'newPassword') {
+      if (!value) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'Password is required' }
+              : field,
+          ),
+        )
+      } else if (value.length < 8) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? {
+                  ...field,
+                  value,
+                  errorMsg: 'Password must be at least 8 characters',
+                }
+              : field,
+          ),
+        )
+      } else {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name ? { ...field, value, errorMsg: '' } : field,
+          ),
+        )
+      }
+    }
+    if (name === 'confirmPassword') {
+      const passwordField = formData.find(
+        (field) => field.name === 'newPassword',
+      )
+      if (passwordField && value !== passwordField.value) {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name
+              ? { ...field, value, errorMsg: 'Passwords do not match' }
+              : field,
+          ),
+        )
+      } else {
+        setFormData((prevFormData) =>
+          prevFormData.map((field) =>
+            field.name === name ? { ...field, value, errorMsg: '' } : field,
+          ),
+        )
+      }
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const hasEmptyField = formData.some((field) => !field.value.trim())
+    if (hasEmptyField) {
+      // Set error messages for empty fields
+      setFormData((prevFormData) =>
+        prevFormData.map((field) =>
+          !field.value.trim()
+            ? { ...field, errorMsg: `${field.placeholder} is required` }
+            : field,
+        ),
+      )
+      window.scroll(0, 300)
+      return
+    }
+  }
+
   return (
     <section className=" flex flex-col gap-[15px] bg-search p-[30px] xl:p-[50px] dxl:gap-[30px]">
       <p className="text-display-11 dxl:text-display-13">Account Information</p>
@@ -6,63 +209,55 @@ function AccountInfoBlock() {
         <p className="text-display-5 leading-none dxl:text-display-16">
           Account Details
         </p>
-        <section className="flex h-auto w-full flex-wrap gap-4">
-          <section className="h-auto shrink-0 grow basis-[200px] opacity-100 sm:basis-[250px] lg:basis-[280px] xl:basis-[340px]">
-            <input
-              type="text"
-              id="first_name"
-              name="first_name"
-              // value={formData?.first_name || ''}
-              placeholder="First Name*"
-              // onChange={handleChange}
-              className="selection: h-[40px] w-full appearance-none bg-textPrimary px-3 py-2 text-display-3 leading-tight focus:border-none focus:outline-none focus:ring-0 dxl:h-[50px] dxl:text-display-6"
-            />
-            {/* {firstNameError && (
-                <p className="mt-1 text-sm text-red-500">{firstNameError}</p>
-              )} */}
-          </section>
-          <section className="h-auto shrink-0 grow basis-[200px] opacity-100 sm:basis-[250px] lg:basis-[280px] xl:basis-[340px]">
-            <input
-              type="text"
-              id="first_name"
-              name="first_name"
-              // value={formData?.first_name || ''}
-              placeholder="Last Name*"
-              // onChange={handleChange}
-              className="selection: h-[40px] w-full appearance-none bg-textPrimary px-3 py-2 text-display-3 leading-tight focus:border-none focus:outline-none focus:ring-0 dxl:h-[50px] dxl:text-display-6"
-            />
-            {/* {firstNameError && (
-                <p className="mt-1 text-sm text-red-500">{firstNameError}</p>
-              )} */}
-          </section>
-          <section className="h-auto shrink-0 grow basis-[200px] opacity-100 sm:basis-[250px] lg:basis-[280px] xl:basis-[340px]">
-            <input
-              type="text"
-              id="first_name"
-              name="first_name"
-              // value={formData?.first_name || ''}
-              placeholder="Email Address*"
-              // onChange={handleChange}
-              className="selection: h-[40px] w-full appearance-none bg-textPrimary px-3 py-2 text-display-3 leading-tight focus:border-none focus:outline-none focus:ring-0 dxl:h-[50px] dxl:text-display-6"
-            />
-            {/* {firstNameError && (
-                <p className="mt-1 text-sm text-red-500">{firstNameError}</p>
-              )} */}
-          </section>
-          <section className="h-auto shrink-0 grow basis-[200px] opacity-100 sm:basis-[250px] lg:basis-[280px] xl:basis-[340px]">
-            <input
-              type="text"
-              id="first_name"
-              name="first_name"
-              // value={formData?.first_name || ''}
-              placeholder="Phone Number*"
-              // onChange={handleChange}
-              className="selection: h-[40px] w-full appearance-none bg-textPrimary px-3 py-2 text-display-3 leading-tight focus:border-none focus:outline-none focus:ring-0 dxl:h-[50px] dxl:text-display-6"
-            />
-            {/* {firstNameError && (
-                <p className="mt-1 text-sm text-red-500">{firstNameError}</p>
-              )} */}
-          </section>
+        <section className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+          {formData.map((field, index) => {
+            return (
+              <section
+                key={index}
+                className={`h-auto ${
+                  index === formFields.length - 1 && istabScreen
+                    ? 'mt-16'
+                    : 'mt-0'
+                } font-sans text-display-3 opacity-100`}
+              >
+                {index === formFields.length - 2 && (
+                  <p className="mb-4 mt-7 text-display-5 leading-normal dxl:mb-7 dxl:mt-[50px] dxl:text-display-16">
+                    Change Password
+                  </p>
+                )}
+                <input
+                  type={
+                    field?.name === 'newPassword' ||
+                    field?.name === 'confirmPassword'
+                      ? 'password'
+                      : 'text'
+                  }
+                  id={field.name}
+                  name={field.name}
+                  value={field?.value || ''}
+                  placeholder={`${field.placeholder}*`}
+                  onChange={handleChange}
+                  className="selection: h-[40px] w-full appearance-none bg-textPrimary px-3 py-2 text-display-3 leading-tight focus:border-none focus:outline-none focus:ring-0 dxl:h-[50px] dxl:text-display-6"
+                />
+                {field?.errorMsg && (
+                  <p className="mt-1 text-sm text-red-500">{field?.errorMsg}</p>
+                )}
+              </section>
+            )
+          })}
+        </section>
+        <section
+          className="relative mt-2 flex h-8 w-[120px] items-center font-sans text-display-1 sm:h-[40px] sm:w-[150px] sm:text-display-4 lg:mt-[15px] xl:h-[53px] xl:w-[220px] xl:text-display-17 dxl:mt-[20px]"
+          onClick={handleSubmit}
+        >
+          <div className="absolute bottom-0 h-[29px] w-[117px] border-[0.5px] border-textSecondary sm:h-[37px] sm:w-[147px] xl:h-[50px] xl:w-[217px]"></div>
+          <div className="absolute right-0 h-[29px] w-[117px] border-[0.5px] border-textSecondary sm:h-[37px] sm:w-[147px] xl:h-[50px] xl:w-[217px]"></div>
+          <button
+            type="submit"
+            className="relative flex w-full items-center justify-center xl:h-[47px]"
+          >
+            Save Changes
+          </button>
         </section>
       </section>
     </section>
